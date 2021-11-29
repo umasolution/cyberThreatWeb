4f22736ecf0926ccf9a69e567aa5db91bbaae7ca
Swarnavalli-shan <80419253+Swarnavalli-shan@users.noreply.github.com>
2021-11-28 20:02:43 -0500

    Icon changed to white

--- src/layouts/DashboardLayout/MinivariantNavBar/index.js
+++ src/layouts/DashboardLayout/MinivariantNavBar/index.js
@@ -305,7 +305,7 @@ export default function MinivariantNavBar() {
             <ListItem button >
               {/*<ListItemIcon className={classes.icon}><SvgIcon component={AlertIcon} viewBox="0 0 600 476.6" fontSize="large" className={classes.svgIcon}/></ListItemIcon>
                */}
-              <ListItemIcon className={classes.icon}><Icon><img src="/static/alert.png" /></Icon></ListItemIcon>
+              <ListItemIcon className={classes.icon}><Icon><img src="/static/Integrations.png" /></Icon></ListItemIcon>
               <ListItemText>
                 <Typography variant="h4" gutterBottom className={classes.navText}>All Integrations</Typography>
               </ListItemText>
