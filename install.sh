f00b6f51a50b5d6c82760cd93aefeadd6285b66d
Jayesh Patel <umasolution@gmail.com>
2022-05-01 16:58:33 -0400

    Added installation step

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Require to install gsutils
+# sudo apt-get install apt-transport-https ca-certificates gnupg -y
+# echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+# curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+# sudo apt-get update
+# sudo apt-get install google-cloud-cli -y
+# sudo gcloud auth activate-service-account --key-file elated-channel-341618-c56acfab2cf3.json
+
+
+var1=$1
+sed -i "s/Niahsecurity.online/$var1/g" src/index.js
+sudo apt-get install curl -y
+curl -sL https://deb.nodesource.com/setup_17.x -o /tmp/nodesource_setup.sh
+sudo bash /tmp/nodesource_setup.sh
+sudo apt-get install -y nodejs
+sudo apt install -y build-essential
+export NODE_OPTIONS=--openssl-legacy-provider
+npm run build
+date_now=$(date "+%F-%H-%M-%S")
+tar -cvf /tmp/$date_now.tar build
+sudo gsutil cp /tmp/$date_now.tar gs://niah-build/
+
