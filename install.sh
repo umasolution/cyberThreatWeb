8f765e46924716621c2a726efe289c3b4e6c8236
jays patel <umasolution@gmail.com>
2022-05-02 16:08:59 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,4 +20,4 @@ export NODE_OPTIONS=--openssl-legacy-provider
 npm run build
 date_now=$(date "+%F-%H-%M-%S")
 tar -cvf /tmp/niahweb_$date_now.tar build
-tar -cvf /tmp/latest.tar build
+tar -cvf /tmp/niahweb_latest.tar build
